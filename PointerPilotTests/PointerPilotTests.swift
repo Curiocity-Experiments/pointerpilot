6fa0297ba72097224d3603c12d1615317f0208c1
Curiocity Developer <randall@curiocity.rocks>
2024-12-01 14:59:22 -0500

    Initial Commit

---
+++ PointerPilotTests/PointerPilotTests.swift
@@ -0,0 +1,16 @@
+//
+//  PointerPilotTests.swift
+//  PointerPilotTests
+//
+//  Created by Randall Noval on 12/1/24.
+//
+
+import Testing
+
+struct PointerPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
